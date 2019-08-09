8ce428a7976d40e5f82db1e1e451303278e758b3
K Hong <albtrossfacefall@gmail.com>
2019-08-09 05:52:14 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -30,7 +30,7 @@ def find_min_value(array)
   end
 
   count=0
-  while count<=array.length do
+  while count<array.length do
     if (array[count]-smallest<=0)
       smallest = array[count]
     end
